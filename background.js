aa367f0476db019df345f6ee8f2bbeaf9149437a
Frank Kilkelly <frank.kilkelly@gmail.com>
2019-03-22 10:12:06 +0100

    Save nanoAddressCache in localstorage instead of locally in script. Receive messages from content script

--- background.js
+++ background.js
@@ -1,35 +1,40 @@
-var nanoAddressCache = new Map()
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  var tab = sender.tab
+  var nanoAddress = request.nanoAddress
 
-// ---------------------------------------------------
-
-// When tab updated
-chrome.tabs.onUpdated.addListener(function (tabId, _, tab) {
-  chrome.tabs.sendMessage(tabId, {text: 'nano_address'}, function (nanoAddress) {
-    if (/^(xrb_|nano_)[13][13-9a-km-uw-z]{59}$/.test(nanoAddress)) {
-      nanoAddressCache.set(tabId, {
-        url: tab.url,
-        nanoAddress: nanoAddress
-      })
-      chrome.browserAction.setIcon({
-        path: 'images/nano-donate-active-128.png',
-        tabId: tabId
-      })
-    } else {
-      nanoAddressCache.set(tabId, {
-        url: tab.url,
-        nanoAddress: ''
-      })
-      chrome.browserAction.setIcon({
-        path: 'images/nano-donate-inactive-128.png',
-        tabId: tabId
-      })
-    }
-  })
+  // Only continue if URL doesn't start with "chrome://"
+  if (!tab.url.startsWith('chrome://')) {
+    chrome.storage.local.get({nanoAddressCache: {}}, function ({ nanoAddressCache }) {
+      // Valid Nano address found so add tab details to cache
+      if (/^(xrb_|nano_)[13][13-9a-km-uw-z]{59}$/.test(nanoAddress)) {
+        nanoAddressCache[tab.id] = {
+          url: tab.url,
+          nanoAddress
+        }
+        chrome.storage.local.set({ nanoAddressCache })
+        chrome.browserAction.setIcon({
+          path: 'images/nano-donate-active-128.png',
+          tabId: tab.id
+        })
+      // No or invalid Nano address found so remove tab details from cache
+      } else {
+        delete nanoAddressCache[tab.id]
+        chrome.storage.local.set({ nanoAddressCache })
+        chrome.browserAction.setIcon({
+          path: 'images/nano-donate-inactive-128.png',
+          tabId: tab.id
+        })
+      }
+    })
+  }
 })
 
 // ---------------------------------------------------
 
 // When tab removed (closed)
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-  nanoAddressCache.delete(tabId)
+  chrome.storage.local.get({nanoAddressCache: {}}, function ({ nanoAddressCache }) {
+    delete nanoAddressCache[tabId]
+    chrome.storage.local.set({nanoAddressCache: nanoAddressCache})
+  })
 })
