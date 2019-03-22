266872a61e28bbf867f4ecc45e6b3d4388b92031
Frank Kilkelly <frank.kilkelly@gmail.com>
2019-03-22 10:10:44 +0100

    Have content script send message to background script, instead of vice versa

--- getNanoMetaTag.js
+++ getNanoMetaTag.js
@@ -1,12 +1,9 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.text === 'nano_address') {
-    var nanoAddress
-    // Search for 'nano' meta tag in document
-    Array.from(document.getElementsByTagName('meta')).filter(function (metaTag) {
-      if (metaTag.getAttribute('name') === 'nano') {
-        nanoAddress = metaTag.getAttribute('content')
-      }
-    })[0]
-    sendResponse(nanoAddress)
+var nanoAddress
+// Search for 'nano' meta tag in document
+Array.from(document.getElementsByTagName('meta')).filter(function (metaTag) {
+  if (metaTag.getAttribute('name') === 'nano') {
+    nanoAddress = metaTag.getAttribute('content')
   }
-})
+})[0]
+
+chrome.runtime.sendMessage({ nanoAddress })
