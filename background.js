fedc3a7084948fa556af9053b3d8af7be3621534
Frank Kilkelly <frank.kilkelly@gmail.com>
2019-04-30 19:59:21 +0200

    v1.2.0

--- background.js
+++ background.js
@@ -1,22 +1,19 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  var tab = sender.tab
-  var nanoAddress = request.nanoAddress
-
+chrome.runtime.onMessage.addListener(function ({ nanoDonateEntries }, { tab }, sendResponse) {
   // Only continue if URL doesn't start with "chrome://"
   if (!tab.url.startsWith('chrome://')) {
     chrome.storage.local.get({nanoAddressCache: {}}, function ({ nanoAddressCache }) {
-      // Valid Nano address found so add tab details to cache
-      if (/^(xrb_|nano_)[13][13-9a-km-uw-z]{59}$/.test(nanoAddress)) {
+      // Valid Nano address/es found so add tab details to cache
+      if (nanoDonateEntries.length) {
         nanoAddressCache[tab.id] = {
           url: tab.url,
-          nanoAddress
+          nanoDonateEntries
         }
         chrome.storage.local.set({ nanoAddressCache })
         chrome.browserAction.setIcon({
           path: 'images/nano-donate-active-128.png',
           tabId: tab.id
         })
-      // No or invalid Nano address found so remove tab details from cache
+      // No Nano addresses found so remove tab details from cache
       } else {
         delete nanoAddressCache[tab.id]
         chrome.storage.local.set({ nanoAddressCache })
--- getNanoMetaTag.js
+++
@@ -1,9 +0,0 @@
-var nanoAddress
-// Search for 'nano' meta tag in document
-Array.from(document.getElementsByTagName('meta')).filter(function (metaTag) {
-  if (metaTag.getAttribute('name') === 'nano') {
-    nanoAddress = metaTag.getAttribute('content')
-  }
-})[0]
-
-chrome.runtime.sendMessage({ nanoAddress })
